3ae753cb2bb93c3ffec62ae4b7a62793f2af776e
BREZOVSKY_FMC <matus.brezovsky@fabrici.sk>
2024-07-31 14:58:48 +0200

    fixed menu component

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { cilHome } from '@coreui/icons';
+import { IconDirective } from '@coreui/icons-angular';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    IconDirective,
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
-
+  icons = { cilHome };
 }
